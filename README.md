c4aaaa6603b1f8c1d102c06ac5ca0b7560ed2f7b
Pablo Alejandro Costesich <rhfixer@gmail.com>
2013-06-02 01:56:33 -0300

    Adds installation script

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Easily login to a django app using a mobile device (phone, tablet)
 
 This library was inspired by aruseni's `django-qrauth', but uses Django's
 database instead of Redis, and implements a custom authentication backend.
+
---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='django-easylogin',
+    version='0.0.1',
+    author='pcostesi',
+    author_email='pcostesi@ieee.org',
+    packages=['easylogin'],
+    url='https://github.com/pcostesi/django-easylogin',
+    license='BSD licence, see LICENCE.md',
+    description=('Easily login to a django app using a '
+        'mobile device (phone, tablet)'),
+    long_description=open('README.md').read()[:-1],
+    zip_safe=False,
+    install_requires=['qrcode', 'django', 'PIL'],
+)
