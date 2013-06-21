ddfebb8b8b04cb306f5605ecdea80c2d34e1ee20
Pablo Alejandro Costesich <rhfixer@gmail.com>
2013-06-20 23:41:41 -0300

    Fixes user agent filtering

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='django-easylogin',
-    version='0.0.7',
+    version='0.0.7-a',
     author='pcostesi',
     author_email='pcostesi@ieee.org',
     packages=['easylogin'],
