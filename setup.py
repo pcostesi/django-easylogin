1146c0d17742def9548389d577ec47a558303eaa
Pablo Alejandro Costesich <rhfixer@gmail.com>
2014-08-04 01:57:10 -0300

    Use setuptools instead of distribute

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='django-easylogin',
-    version='0.0.8',
+    version='0.1.0',
     author='pcostesi',
     author_email='pcostesi@ieee.org',
     packages=['easylogin'],
